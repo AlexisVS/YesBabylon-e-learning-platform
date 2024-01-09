17a912d517123f940ab4675f9a578f1af6e1b2d8
AlexisVS <alexis.vansan1440@gmail.com>
2024-01-09 09:55:04 +0100

    Creating the InputTransformer component

--- src/app/_components/input-transformer/input-transformer.module.ts
+++ src/app/_components/input-transformer/input-transformer.module.ts
@@ -5,6 +5,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
 	imports: [
@@ -13,7 +14,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 		MatButtonModule,
 		MatFormFieldModule,
 		MatInputModule,
-		ReactiveFormsModule
+		ReactiveFormsModule,
+		MatIconModule
 	],
 	declarations: [
 		InputTransformerComponent
